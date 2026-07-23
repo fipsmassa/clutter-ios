a1523e6bcb073f95f678797bedfe2beb0f7dd06c
Philipp Seibold <info@philipp-seibold.de>
2026-07-23 18:01:24 +0200

    add create flows for topics and bullets

---
+++ Clutter/Views/AddBulletSheet.swift
@@ -0,0 +1,18 @@
+//
+//  AddBulletSheet.swift
+//  Clutter
+//
+//  Created by Philipp Seibold on 23.07.26.
+//
+
+import SwiftUI
+
+struct AddBulletSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBulletSheet()
+}
---
+++ Clutter/Views/AddTopicSheet.swift
@@ -0,0 +1,18 @@
+//
+//  AddTopicSheet.swift
+//  Clutter
+//
+//  Created by Philipp Seibold on 22.07.26.
+//
+
+import SwiftUI
+
+struct AddTopicSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTopicSheet()
+}
